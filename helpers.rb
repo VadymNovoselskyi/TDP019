b8e37e2a8adc5d10061e320a8b0dd7967b4f99cd
Vadym Novoselskyi <novoselskyi.vadim@gmail.com>
2026-05-07 16:04:53 +0200

    filter out lookup nodes from variable assignment checks

--- helpers.rb
+++ helpers.rb
@@ -4,20 +4,20 @@ require "./types/class.rb"
 # Checks if the lhs type is equal to the rhs type (both need to be actual nodes) 
 def is_equal_types(lhs, rhs)
   if (lhs.eval_type().is_a?(ClassType) && rhs.eval_type().is_a?(ClassType))
-    puts "Comparing class types: #{lhs.eval_type().get_class_name()} and #{rhs.eval_type().get_class_name()}"
+    # puts "Comparing class types: #{lhs.eval_type().get_class_name()} and #{rhs.eval_type().get_class_name()}"
     raise "Cannot compare two class types (neither is instance): #{lhs.eval_type().get_class_name()} and #{rhs.eval_type().get_class_name()}"
   end
 
   if (is_class_type(lhs) && is_class_type(rhs))
-    puts "Comparing two class-related types: #{lhs.eval_type()} and #{rhs.eval_type()}"
+    # puts "Comparing two class-related types: #{lhs.eval_type()} and #{rhs.eval_type()}"
     class_type, class_instance = lhs.eval_type().is_a?(ClassType) ? [lhs.eval_type(), rhs] : [rhs.eval_type(), lhs]
     class_name = class_type.get_class_name()
     class_instance = class_instance.is_a?(ClassInstantiation) ? class_instance.evaluate() : class_instance
-    puts "Comparing class instance #{class_instance.get_class_name()} to class name #{class_name}"
+    # puts "Comparing class instance #{class_instance.get_class_name()} to class name #{class_name}"
     return class_instance.is_subclass_of(class_name)
   end
 
-  puts "Comparing types: #{lhs.eval_type()} and #{rhs.eval_type()}"
+  # puts "Comparing types: #{lhs.eval_type()} and #{rhs.eval_type()}"
   return lhs.eval_type() == rhs.eval_type()
 end 
 # Asumes everything is a Variable 
@@ -27,23 +27,23 @@ end
 # .eval_type() returns INSTANCE of ClassType (.is_a(ClassType) returns true): 
 # Call .get_class_name() to get the class name (string) 
 # 
-# .eval_type() return ClassInstantiation: 
+# .eval_type() returns ClassInstantiation: 
 # Call .evaluate() to get the class instance, then call .get_class_name() to get the class name (string)
 # 
-# .eval_type() return ClassInstanceType:
+# .eval_type() returns ClassInstanceType:
 # Call .get_class_name() to get the class name (string)
 # 
 # If both vars are either ClassType/ClassInstantiation/ClassInstanceType:
 # Call is_subclass_of() on one of the variables with name (string) of another
 
- def is_class_type(node)
+def is_class_type(node)
   # puts "Checking if node is a class type: #{node.eval_type()}"
   return node.eval_type().is_a?(ClassType) || node.eval_type() == ClassInstanceType || node.eval_type() == ClassInstantiation
 end
 
 def is_assignable_to(node, type) 
   type = type.is_a?(ClassType) ? type.get_class_name() : type
-  # puts "Checking if #{node.eval_type()} is assignable to #{type}"
+  # puts "Checking if #{node} is assignable to #{type}"
     
   if is_class_type(node)
     # puts "Node is a class type, checking if it is a subclass of #{type} (#{node.is_subclass_of(type)})"
@@ -52,4 +52,12 @@ def is_assignable_to(node, type)
 
   # puts "Node is not a class type, comparing types: #{node.eval_type()} and #{type}"
   return node.eval_type() == type
+end
+
+def is_lookup_node(node)
+  return node.is_a?(VariableLookup) ||
+   node.is_a?(ClassAttributeLookup) || 
+   node.is_a?(ClassAttributeModification) || 
+   node.is_a?(FunctionCall) ||
+   node.is_a?(ClassMethodCall)
 end
--- types/variable.rb
+++ types/variable.rb
@@ -6,7 +6,7 @@ class Variable < BaseNode
   def initialize(type_class, name, value = nil)
     
     # puts "Checking type of variable assignment: name: #{name}; value #{value.inspect()}", " type #{type_class.inspect()}"
-    if value != nil && value.class != ClassAttributeLookup && !is_assignable_to(value, type_class)
+    if value != nil && !is_lookup_node(value) && !is_assignable_to(value, type_class)
       raise "Trying to assign #{value.evaluate()} to a variable of type #{type_class}"
     end
     
@@ -17,16 +17,9 @@ class Variable < BaseNode
   
   def reassign(new_value)
     # puts "Reassigning variable #{@name} to new value #{new_value.inspect()} of type #{new_value.eval_type()}"
-    if (@type_class.class == ClassType && (new_value.eval_type() == ClassInstantiation || new_value.eval_type() == ClassInstanceType))
-      if (!new_value.evaluate().is_subclass_of(@type_class.get_class_name()))
-        raise "Trying to assign #{new_value.evaluate()} to a variable of type #{@type_class.get_class_name()}"
-      end
-
-      @value = new_value.evaluate()
-      return
-    end
-    if (new_value.eval_type() != @type_class)
-      raise "Trying to assign #{new_value.eval_type()} to a variablel of type #{@type_class}"
+    new_value = new_value.evaluate() if is_class_type(new_value)
+    if !is_assignable_to(new_value, @type_class)
+      raise "Trying to assign #{new_value.evaluate()} to a variable of type #{@type_class}"
     end
     @value = new_value
   end
@@ -73,7 +66,7 @@ class VariableLookup < BaseNode
 end
 
 class Reassign < BaseNode
-  attr_accessor :name
+  attr_accessor :name, :new_value
   
   def initialize(name, new_value)
     @name = name
